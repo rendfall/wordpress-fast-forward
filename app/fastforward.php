73a71b46a140019a605a0eb973672bd7dad439c4
rendfall@gmail.com <rendfall@gmail.com>
2016-08-14 19:08:36 +0200

    Add some phpdoc + clean code

--- app/fastforward.php
+++ app/fastforward.php
@@ -1,24 +1,32 @@
 <?php
 
 class FastForward {
+    /**
+     * String the version of FastForward.
+     * 
+     * @var string
+     */
+    public static $VERSION = '1.1';
 
     /**
-     * @return string the version of FastForward
+     * Cache for parts of engine.
+     * 
+     * @var array
      */
-    public static function getVersion() {
-        return '1.0';
-    }
-
     protected static $_cache = array();
 
-
+    /**
+     * Helper: Build classname from filename.
+     * 
+     * @param string $filename
+     * @return string
+     */
     protected static function fileToClassName($filename){
         return implode('', array_map('ucfirst', explode('-', $filename)));
     }
 
-
     /**
-     * Build the required object instance
+     * Build the required object instance.
      * 
      * @param string  $object
      * @param boolean $fresh - whether to get a fresh copy; will not be cached and won't override current copy in cache.
@@ -28,11 +36,11 @@ class FastForward {
             return self::$_cache[$object];
         }
 
-        $class = get_called_class().'_'.self::fileToClassName($object);
+        $class = get_called_class() . '_' . self::fileToClassName($object);
         $class = str_replace('/', '_', $class);
 
         if (!class_exists($class)) {
-            require_once locate_template('app/' . $object . '.php');
+            require_once locate_template(__FILE__ . '/' . $object . '.php');
         }
 
         if ($fresh) {
@@ -40,40 +48,39 @@ class FastForward {
         }
         
         self::$_cache[$object] = new $class;
+
         return self::$_cache[$object];
     }
 
-
     public static function Options($fresh = false){
-        return self::factory('options', $fresh);    
+        return self::factory('options', $fresh);
     }
 
     public static function Posts($fresh = false){
-        return self::factory('posts', $fresh);    
+        return self::factory('posts', $fresh);
     }
 
     public static function Post($fresh = false){
-        return self::factory('post', $fresh);    
+        return self::factory('post', $fresh);
     }
 
     public static function Menus($fresh = false){
-        return self::factory('menus', $fresh);    
+        return self::factory('menus', $fresh);
     }
 
     public static function Helpers($fresh = false){
-        return self::factory('helpers', $fresh);    
+        return self::factory('helpers', $fresh);
     }
 
     public static function PostTypes($fresh = false){
-        return self::factory('posttypes', $fresh);    
+        return self::factory('posttypes', $fresh);
     }
 
     public static function Widgets($fresh = false){
-        return self::factory('widgets', $fresh);    
+        return self::factory('widgets', $fresh);
     }
 
     public static function Shortcodes($fresh = false){
-        return self::factory('shortcodes', $fresh);    
+        return self::factory('shortcodes', $fresh);
     }
 }
-
